09c04e0dd13d367807a75ca136326447158324d7
Elijah-Akaninyene1 <elijahakaninyene66@gmail.com>
2026-06-22 10:51:21 +0100

    Add files via upload

--- assignment.js
+++ assignment.js
@@ -19,15 +19,54 @@ fah(100)
 
 
 function checkentry(age, isvip) {
-    if (isvip === true) {
+     if(isvip === true && age >= 21){
+    return "access grnted: Enjoy the night!"
+    }
+    if (isvip === true || age >= 21) {
         return "access granted: Welcome VIP! immediately "
     }
 
     if (age < 21) {
         return "access denied: Too young!"
     }   
-    if (isvip === true && age > 21){
-    return "access granted: Enjoy the night!"
 }
+console.log(checkentry(25, true))
+
+
+
+
+function calculateDogAge(puppyAge) {
+    let conversioon_rate = 7
+    let dogAge = puppyAge * conversioon_rate;
+    return dogAge + 'years';
+
+
 }
-console.log(checkentry(18, false))
+console.log(calculateDogAge(2, 7))
+console.log(calculateDogAge(9, 7))
+
+
+
+function calCircumference(radius) {
+    let circumference = (2 * Math.PI * radius).toFixed(2);
+    return circumference + 'cm';
+
+
+}
+console.log(calCircumference(78))
+
+
+function Capitlize(firstletter) {
+    return firstletter
+    .split(" ")
+    .map(function(word) {
+        return word.charAt(0).toUpperCase() +
+        word.slice(1);
+    })    
+    .join(" ")
+}
+console.log(Capitlize("Hello i am a student of digital world tech academy"))
+
+
+
+
